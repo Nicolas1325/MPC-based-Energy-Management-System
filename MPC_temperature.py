1337dcdc7ff68d0a85d945c7cf8d6614e5c69985
nsierro <sierronicolas@gmail.com>
2017-11-27 12:23:53 +0100

    a

--- MPC_temperature.py
+++ MPC_temperature.py
@@ -24,7 +24,7 @@ class EMS_MPC_temperature(EnergyManagementSystem):
 
     def initModelParameters(self):
 
-        self.T = 96
+        self.T = 4*3600/self.ems_dt
         # self.N=96
 
         # self.surface=120
@@ -51,9 +51,6 @@ class EMS_MPC_temperature(EnergyManagementSystem):
         self.R = int_interest.thermalProp.resistiveParameters[0]
         self.cw = int_interest.thermalProp.capacitiveParam[0]
 
-        print(self.R)
-        print(self.cw)
-
         self.p_air = 1.292
         self.Vroom = 192
         self.cm_air = 1005
@@ -62,45 +59,49 @@ class EMS_MPC_temperature(EnergyManagementSystem):
         # self.upper_bond=np.array[[26],[np.inf]]
         # self.lower_bond = np.array[[20], [-np.inf]]
 
-        self.t_wall = 15  # to define
+        self.t_wall = -300  # to define
 
-    def mpc_control(self, temp_cur, temp_wall_cur, temp_ext, temp_constr, elec_price):
+
+
+        self.battery_energy=0
+
+    def mpc_control(self, temp_cur, temp_wall_cur, temp_ext, elec_price, temp_constr):
 
         x = cvxpy.Variable(2, self.T + 1)
         u = cvxpy.Variable(1, self.T)
         epsi = cvxpy.Variable(1, self.T + 1)
-        q = 100
+        q = 10000
 
         A, B, B1 = self.get_model()
 
+        print("temp ext" + str(temp_ext))
         totalcost = 0.0
         constr = []
         for t in range(self.T):
-
-            min_bound = temp_constr.get_min(t)
-            max_bound = temp_constr.get_max(t)
-
-            totalcost += elec_price[t]*u[t]
-            totalcost += cvxpy.quad_form(epsi[t + 1], q)
+            totalcost += elec_price[t]*u[t] #+ cvxpy.quad_form(epsi[t + 1], q)
             constr += [x[:, t + 1] == A * x[:, t] + B * u[t] + B1*temp_ext[t]]
-            constr += [x[0, t + 1] <= max_bound+epsi[t + 1]]
-            constr += [x[0, t + 1] >= min_bound-epsi[t + 1]]
+
+            min_bound = temp_constr.get_min(self.current_time+t*self.ems_dt)
+            max_bound = temp_constr.get_max(self.current_time+t*self.ems_dt)
+            print("min_bound "+str(min_bound))
+            print("max_bound "+str(max_bound))
+
+            constr += [x[0, t + 1] <= max_bound]#+epsi[t + 1]]
+            constr += [x[0, t + 1] >= min_bound]#-epsi[t + 1]]
             constr += [u[t] <= 5000]
             constr += [u[t] >= 0]
-
         constr += [x[:, 0] == np.array([[temp_cur],[temp_wall_cur]])]
-        constr += [epsi[0] == 30]
+        #constr += [epsi[0] == 50]
+
         prob = cvxpy.Problem(cvxpy.Minimize(totalcost), constr)
-
-        prob.solve(verbose=False)
-
+        prob.solve(verbose=False, feastol=1e-4)
         if prob.status == cvxpy.OPTIMAL:
-
             p_opt = np.array(u.value[0, :]).flatten()
-            print(p_opt)
+            epsi_v = np.array(epsi.value[0, :]).flatten()
+            print("epsi "+ str(epsi_v))
             return p_opt[0]
         else:
-            print("Problem infeasible")
+            print("problem infeasible")
 
     def get_model(self):
         Acont=np.array([[-1/(self.cr*self.R), 1/(self.cr*self.R)],
@@ -129,6 +130,7 @@ class EMS_MPC_temperature(EnergyManagementSystem):
             obj_room = self.building_data.room(r_id)
 
             temp_constr = obj_room.get_comfort_constraint(EMS_COMFORT_temperature)
+
             temp_cur = obj_room.get_comfort_value(EMS_COMFORT_temperature)
 
             #for r_id in self.building_data.room_ids_list():
@@ -136,20 +138,34 @@ class EMS_MPC_temperature(EnergyManagementSystem):
              #       outside = self.building_data.room(r_id)
             outside = self.building_data.room(13)
 
+            elec_price = self.get_elec_price((self.current_time, self.current_time+self.T*self.ems_dt, self.ems_dt))
+
+            print("elec_price" + str(elec_price))
+            #ext_temperature = [15 for i in range(0, self.T*self.ems_dt, self.ems_dt)]
             ext_temperature = outside.get_forecast(EMS_COMFORT_temperature,
-                                                   (self.current_time, self.current_time+self.T, self.ems_dt),
+                                                   (0, 0+self.T*self.ems_dt, self.ems_dt),
                                                    self.environment_data)
-            elec_price = self.get_elec_price((self.current_time, self.current_time+self.T, self.ems_dt))
-            print("temp cur =" + str(temp_cur))
+            #print("temp cur =" + str(temp_cur))
+
 
             if temp_cur != None:
 
-                p_opt = self.mpc_control(temp_cur, self.t_wall, ext_temperature, temp_constr,elec_price)
+                if self.t_wall == -300: #Initialise T wall
+                    self.t_wall=(ext_temperature[0]+temp_cur)/2
 
-                print(ext_temperature)
-                print((self.starting_day * 24 * 60 * 60)%self.current_time)
-                self.t_wall=(temp_cur+ext_temperature[(self.starting_day * 24 * 60 * 60)%self.current_time])/2 #TO CHANGE : design an observer
-                command_hvac = self.hvac_set_point(hvac_id, p_opt)
+                p_opt = self.mpc_control(temp_cur, self.t_wall, ext_temperature, elec_price, temp_constr)
+
+                [A, B1, B2] = self.get_model()
+                T=np.array([[temp_cur], [self.t_wall]])
+                #print("temp_cur "+ str(temp_cur) + "t_wall" + str(self.t_wall))
+                #print("ext_t " + str(ext_temperature))
+                print("p_opt"+str(p_opt))
+
+                self.t_wall=(np.dot(A,T)+np.dot(B1,p_opt)+np.dot(B2,ext_temperature[0])).item(1)
+                #self.battery_energy = self.battery_energy + optimal_Pb * self.ems_dt/3600.0
+                #print(ext_temperature)
+                command_hvac = self.hvac_set_point(hvac_id, p_opt/5000.0)
+                #command_battery = self.battery_set_point(battery_id, optimal_Pb)
                 commands_set.append(command_hvac)
 
         return commands_set
